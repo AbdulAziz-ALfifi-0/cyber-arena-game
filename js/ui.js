83001bb369f9aec1aaa73d2c7e8435db8674ee56
A-Z <s446001042@uqu.edu.sa>
2026-03-06 02:41:40 +0300

    Delete js directory

--- js/ui.js
+++
@@ -1,8 +0,0 @@
-function updateUI(){
-
-document.getElementById("score").textContent = score
-document.getElementById("level").textContent = level
-document.getElementById("xp").textContent = xp
-document.getElementById("health").textContent = player.health
-
-}
