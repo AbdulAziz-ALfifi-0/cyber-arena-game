04c23d2d5b8f37766fa161ac5c02863fe2f115a8
A-Z <s446001042@uqu.edu.sa>
2026-03-06 02:25:38 +0300

    Add updateUI function to update game stats

---
+++ js/ui.js
@@ -0,0 +1,8 @@
+function updateUI(){
+
+document.getElementById("score").textContent = score
+document.getElementById("level").textContent = level
+document.getElementById("xp").textContent = xp
+document.getElementById("health").textContent = player.health
+
+}
